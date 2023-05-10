dd54b06e1169bdc22f011d9c2863f9b82592a4cf
Lexuz <90095821+Lexuz17@users.noreply.github.com>
2023-05-11 00:03:45 +0700

    Add welcome page
    Menambahkan file swift dan landing page beserta sign in page tetapi blm di coba run karna device

---
+++ JEF_Mentals/login/loginApp.swift
@@ -0,0 +1,17 @@
+//
+//  loginApp.swift
+//  login
+//
+//  Created by Jason Susanto on 9/5/23.
+//
+
+import SwiftUI
+
+@main
+struct loginApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
